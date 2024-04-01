5aa404503203ba4da270249c0f4c0170d3da3c43
zackynz <nurasfanz@gmail.com>
2024-04-01 16:02:24 +0800

    memberikan tambahan  untuk menampilkan data mahasiswa

--- activitystruct_0065.cpp
+++ activitystruct_0065.cpp
@@ -31,14 +31,17 @@ int main(){
         cin >> mhs[i].umur;
         cin.ignore(1,'n');
     }
+
     for (int i=0;i<3;i++){
-         cout << endl;
+        cout << endl;
+        cout << "Data Mahasiswa ke-"<<(i+1)<<" : "<<endl;
         cout << "\n NIM : " << mhs[i].nim;
         cout << "\n Nama : " << mhs[i].nama;
         cout << "\n Alamat : " ;
         cout << "\n \t Desa : "<< mhs[i].alamat.desa;
         cout << "\n \t Kota : "<< mhs[i].alamat.kota;
         cout << "\n Umur : " << mhs[i].umur;
+        cout<<endl;
 
     }
 }
