eacef21f83c30c49d20687ff991717d0250b4ac9
zackynz <nurasfanz@gmail.com>
2024-04-01 15:57:15 +0800

    memberikan tampilan data

--- activitystruct_0065.cpp
+++ activitystruct_0065.cpp
@@ -16,6 +16,7 @@ struct Mahasiswa{
 int main(){
     Mahasiswa mhs[3];
     for (int i=0;i<3;i++){
+        cout <<"Data ke-"<<(i+1)<<":"<<endl;
         cout << "Nomor Mahasiswa : ";
         getline(cin,mhs[i].nim);
         cout << "Nama Mahasiswa : ";
