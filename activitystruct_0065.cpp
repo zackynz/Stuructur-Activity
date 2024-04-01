15fb9a5577ff1325f8539d056535314116ca5eb7
zackynz <nurasfanz@gmail.com>
2024-04-01 15:47:47 +0800

    menambahkan array

--- activitystruct_0065.cpp
+++ activitystruct_0065.cpp
@@ -14,30 +14,19 @@ struct Mahasiswa{
 };
 
 int main(){
-    Mahasiswa mhs;
-    cout << "Nomor Mahasiswa : ";
-    getline(cin , mhs.nim);
-    cin >> mhs.nim;
-    cout << "Nama Mahasiswa : ";
-    getline(cin , mhs.nama);
-    cin >> mhs.nama;
-
-    cout << "Alamat Mahasiswa : ";
-    cout << "\t Nama Desa : ";
-    cin >> mhs.alamat.desa;
-    cout << "\t Nama Kota : ";
-    cin >> mhs.alamat.kota;
-  
-
-    cout << "Umur Mahasiswa : ";
-    cin >> mhs.umur;
-
-    cout << endl;
-    cout << "\n NIM : " << mhs.nim;
-    cout << "\n Nama : " << mhs.nama;
-    cout << "\n Alamat : " ;
-    cout << "\n \t Desa : "<< mhs.alamat.desa;
-    cout << "\n \t Kota : "<< mhs.alamat.kota;
-    cout << "\n Umur : " << mhs.umur;
-
-}
+    Mahasiswa mhs[3];
+    for (int i=0;i<3;i++){
+        cout << "Nomor Mahasiswa : ";
+        getline(cin,mhs[i].nim);
+        cout << "Nama Mahasiswa : ";
+        getline(cin,mhs[i].nama);
+        
+        cout << "Alamat Mahasiswa : "<<endl;
+        cout << "\t Nama Desa : ";
+        cin >> mhs[i].alamat.desa;
+        cout << "\t Nama Kota : ";
+        cin >> mhs[i].alamat.kota;
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+    }
+   
