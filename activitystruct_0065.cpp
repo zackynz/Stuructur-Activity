7c2f2ef2763049053c1912133b3c9eabc7419ac3
zackynz <nurasfanz@gmail.com>
2024-04-01 15:14:08 +0800

    strukrut tahap 1

--- activitystruct_0065.cpp
+++ activitystruct_0065.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-
 using namespace std;
-
-struct DetailAlamat{
-    string desa;
-    string kota;
-};
 
 struct Mahasiswa{
     string nim;
     string nama;
-    DetailAlamat alamat;
+    string alamat;
     int umur;
 };
 
+int main(){
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+    cout << "Alamat Mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n NIM : " << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n Alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
+
+}
