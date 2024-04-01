49ab0ca896ee2344d695ae094f474f2f58607fee
zackynz <nurasfanz@gmail.com>
2024-04-01 15:55:29 +0800

    memberikan cin ignore

--- activitystruct_0065.cpp
+++ activitystruct_0065.cpp
@@ -28,6 +28,7 @@ int main(){
         cin >> mhs[i].alamat.kota;
         cout << "Umur Mahasiswa : ";
         cin >> mhs[i].umur;
+        cin.ignore(1,'n');
     }
     for (int i=0;i<3;i++){
          cout << endl;
